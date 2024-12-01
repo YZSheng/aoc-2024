f4c8d77d5f7191e7e35fb041508e4c658e6d0755
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-01 13:26:33 +0800

    minor renaming

--- src/aoc2024/core.clj
+++ src/aoc2024/core.clj
@@ -1,7 +1,7 @@
 (ns aoc2024.core
   (:require [clojure.string :as str]))
 
-(defn read-lines-of-numbers [input]
+(defn read-lines [input]
   (->> input
        slurp
        str/split-lines))
--- src/aoc2024/day01/solution.clj
+++ src/aoc2024/day01/solution.clj
@@ -1,6 +1,6 @@
 (ns aoc2024.day01.solution
   (:require
-   [aoc2024.core :refer [read-lines-of-numbers]]
+   [aoc2024.core :refer [read-lines]]
    [clojure.string :as str]))
 
 (def sample-input "3   4
@@ -15,15 +15,6 @@
 (defn transpose [m]
   (apply mapv vector m))
 
-(->> input
-     (map #(str/split % #"\s+"))
-     (map (fn [x] (map #(Integer/parseInt %) x)))
-     transpose
-     (map sort)
-     transpose
-     (map (fn [[a b]] (Math/abs (- a b))))
-     (reduce +))
-
 (defn solve [input]
   (->> input
        (map #(str/split % #"\s+"))
@@ -34,7 +25,8 @@
        (map (fn [[a b]] (Math/abs (- a b))))
        (reduce +)))
 
-(solve (read-lines-of-numbers "resources/day01/input.txt"))
+(solve input)
+(solve (read-lines "resources/day01/input.txt"))
 
 (defn solve2 [input]
   (let [[a b] (->> input
@@ -44,4 +36,5 @@
         freq (frequencies b)]
     (reduce + (map (fn [x] (* x (get freq x 0))) a))))
 
-(solve2 (read-lines-of-numbers "resources/day01/input.txt"))
+(solve2 input)
+(solve2 (read-lines "resources/day01/input.txt"))
