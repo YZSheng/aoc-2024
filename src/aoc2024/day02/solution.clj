bcc78838eb2ca73958f605faae9d7477090f97af
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-02 19:57:52 +0800

    add day02 part2

--- src/aoc2024/day02/solution.clj
+++ src/aoc2024/day02/solution.clj
@@ -11,9 +11,7 @@
 8 6 4 4 1
 1 3 6 7 9"))
 
-sample-input
-
-(defn is-save [pairs]
+(defn is-safe [pairs]
   (or (every? (fn [[a b]] (and (< a b) (>= 3 (- b a)))) pairs)
       (every? (fn [[a b]] (and (> a b) (>= 3 (- a b)))) pairs)))
 
@@ -22,8 +20,28 @@ sample-input
        (map #(str/split % #"\s+"))
        (map (fn [x] (map #(Integer/parseInt %) x)))
        (map (fn [x] (partition 2 1 x)))
-       (filter is-save)
+       (filter is-safe)
        count))
 
 (solve1 sample-input)
 (solve1 (read-lines "resources/day02/input.txt"))
+
+(defn sublists-with-one-removed [items]
+  (map-indexed
+   (fn [idx _]
+     (concat (take idx items) (drop (inc idx) items)))
+   items))
+
+(defn is-safe-if-remove-one [nums]
+  (if (is-safe (partition 2 1 nums)) true
+      (some #(is-safe (partition 2 1 %)) (sublists-with-one-removed nums))))
+
+(defn solve2 [input]
+  (->> input
+       (map #(str/split % #"\s+"))
+       (map (fn [x] (map #(Integer/parseInt %) x)))
+       (filter is-safe-if-remove-one)
+       count))
+
+(solve2 sample-input)
+(solve2 (read-lines "resources/day02/input.txt"))
