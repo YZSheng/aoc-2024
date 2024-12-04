863ecfa3bd0bb3db3ff7b5911fdf52790006de9d
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-04 14:31:46 +0800

    minor refactoring

--- src/aoc2024/day04/solution.clj
+++ src/aoc2024/day04/solution.clj
@@ -16,89 +16,38 @@ MXMXAXMASX")
 
 sample-input
 
-
 (def char-map (->> (str/split-lines sample-input)
                    (map (fn [x] (str/split x #"")))))
 
 char-map
 
-(defn check-coord-down [x y m]
-  (and
-   (= "M" (get-in m [x (+ 1 y)]))
-   (= "A" (get-in m [x (+ 2 y)]))
-   (= "S" (get-in m [x (+ 3 y)]))))
-
-
-(defn check-coord-up [x y m]
-  (and
-   (= "M" (get-in m [x (- y 1)]))
-   (= "A" (get-in m [x (- y 2)]))
-   (= "S" (get-in m [x (- y 3)]))))
-
-
-(defn check-coord-right [x y m]
-  (and
-   (= "M" (get-in m [(+ x 1) y]))
-   (= "A" (get-in m [(+ x 2) y]))
-   (= "S" (get-in m [(+ x 3) y]))))
-
-
-(defn check-coord-left [x y m]
-  (and
-   (= "M" (get-in m [(- x 1) y]))
-   (= "A" (get-in m [(- x 2) y]))
-   (= "S" (get-in m [(- x 3) y]))))
-
-
-(defn check-coord-top-right [x y m]
-  (and
-   (= "M" (get-in m [(+ x 1) (- y 1)]))
-   (= "A" (get-in m [(+ x 2) (- y 2)]))
-   (= "S" (get-in m [(+ x 3) (- y 3)]))))
-
-
-(defn check-coord-bottom-right [x y m]
-  (and
-   (= "M" (get-in m [(+ x 1) (+ y 1)]))
-   (= "A" (get-in m [(+ x 2) (+ y 2)]))
-   (= "S" (get-in m [(+ x 3) (+ y 3)]))))
-
-
-(defn check-coord-bottom-left [x y m]
-  (and
-   (= "M" (get-in m [(- x 1) (+ y 1)]))
-   (= "A" (get-in m [(- x 2) (+ y 2)]))
-   (= "S" (get-in m [(- x 3) (+ y 3)]))))
-
-
-(defn check-coord-top-left [x y m]
-  (and
-   (= "M" (get-in m [(- x 1) (- y 1)]))
-   (= "A" (get-in m [(- x 2) (- y 2)]))
-   (= "S" (get-in m [(- x 3) (- y 3)]))))
+(defn check-coord [x y m deltas]
+  (let [coords (map (fn [[dx dy]] [(+ x dx) (+ y dy)]) deltas)
+        chars (map #(get-in m % nil) coords)]
+    (= chars ["M" "A" "S"])))
 
 (defn find-xmas [x y m]
-  (->> [check-coord-up
-        check-coord-down
-        check-coord-left
-        check-coord-right
-        check-coord-top-right
-        check-coord-bottom-right
-        check-coord-bottom-left
-        check-coord-top-left]
-       (filter (fn [f]
-                 (f x y m)))
-       count))
+  (let [directions {:up            [[0 -1] [0 -2] [0 -3]]
+                    :down          [[0 1] [0 2] [0 3]]
+                    :left          [[-1 0] [-2 0] [-3 0]]
+                    :right         [[1 0] [2 0] [3 0]]
+                    :top-right     [[1 -1] [2 -2] [3 -3]]
+                    :bottom-right  [[1 1] [2 2] [3 3]]
+                    :bottom-left   [[-1 1] [-2 2] [-3 3]]
+                    :top-left      [[-1 -1] [-2 -2] [-3 -3]]}]
+    (->> directions
+         vals
+         (filter #(check-coord x y m %))
+         count)))
 
 (defn solve1 [input]
-  (let [char-map (map (fn [x] (str/split x #"")) input)]
+  (let [char-map (mapv #(vec (str/split % #"")) input)]
     (->> (for [x (range (count (first char-map)))
                y (range (count char-map))
-               :when (= "X" (get-in (vec char-map) [x y]))]
+               :when (= "X" (get-in char-map [x y]))]
            [x y])
-         (map (fn [[x y]] (find-xmas x y (vec char-map))))
+         (map (fn [[x y]] (find-xmas x y char-map)))
          (reduce +))))
-
 (solve1 (str/split-lines sample-input))
 
 (solve1 (str/split-lines (read-single-line "resources/day04/input.txt")))
