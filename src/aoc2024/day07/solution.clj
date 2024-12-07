cf45849bd02683155504a933224da433ced1296d
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-07 13:32:18 +0800

    add day07 part2

--- src/aoc2024/day07/solution.clj
+++ src/aoc2024/day07/solution.clj
@@ -1,7 +1,7 @@
 (ns aoc2024.day07.solution
-  (:require [clojure.string :as str]))
+   (:require [clojure.string :as str]))
 
-(def sample-input "190: 10 19
+ (def sample-input "190: 10 19
 3267: 81 40 27
 83: 17 5
 156: 15 6
@@ -11,35 +11,65 @@
 21037: 9 7 18 13
 292: 11 6 16 20")
 
-(defn parse-input [input]
-  (->> (str/split-lines input)
-       (map #(re-seq #"\d+" %))
-       (map (fn [xs] (map #(Long/parseLong %) xs)))))
+ (defn parse-input [input]
+   (->> (str/split-lines input)
+        (map #(re-seq #"\d+" %))
+        (map (fn [xs] (map #(Long/parseLong %) xs)))))
 
-(parse-input sample-input)
+ (parse-input sample-input)
 
 
-(defn evaluate-result [numbers]
-  (let [first-number (first numbers)
-        rest-numbers (rest numbers)]
-    (if (= 2 (count numbers))
-      (= first-number (second numbers))
-      (or (evaluate-result (concat [first-number]
-                                   (vector (* (first rest-numbers) (second rest-numbers)))
-                                   (rest (rest rest-numbers))))
-          (evaluate-result (concat [first-number]
-                                   (vector (+ (first rest-numbers) (second rest-numbers)))
-                                   (rest (rest rest-numbers))))))))
+ (defn evaluate-result [numbers]
+   (let [first-number (first numbers)
+         rest-numbers (rest numbers)]
+     (if (= 2 (count numbers))
+       (= first-number (second numbers))
+       (or (evaluate-result (concat [first-number]
+                                    (vector (* (first rest-numbers) (second rest-numbers)))
+                                    (rest (rest rest-numbers))))
+           (evaluate-result (concat [first-number]
+                                    (vector (+ (first rest-numbers) (second rest-numbers)))
+                                    (rest (rest rest-numbers))))))))
 
-(evaluate-result '(190 10 19))
-(evaluate-result '(3267 81 40 27))
+ (evaluate-result '(190 10 19))
+ (evaluate-result '(3267 81 40 27))
 
-(->> (parse-input sample-input)
-     (filter evaluate-result)
-     (map first)
-     (reduce +))
+ (defn solve1 [input]
+   (->> (parse-input input)
+        (filter evaluate-result)
+        (map first)
+        (reduce +)))
 
-(->> (parse-input (slurp "resources/day07/input.txt"))
-     (filter evaluate-result)
-     (map first)
-     (reduce +))
+ (solve1 sample-input)
+
+ (solve1 (slurp "resources/day07/input.txt"))
+
+;; part 2
+
+ (defn merge-numbers [a b]
+   (Long/parseLong (str a b)))
+
+ (defn evaluate-result-2 [numbers]
+   (let [first-number (first numbers)
+         rest-numbers (rest numbers)]
+     (if (= 2 (count numbers))
+       (= first-number (second numbers))
+       (or (evaluate-result-2 (concat [first-number]
+                                      (vector (* (first rest-numbers) (second rest-numbers)))
+                                      (rest (rest rest-numbers))))
+           (evaluate-result-2 (concat [first-number]
+                                      (vector (+ (first rest-numbers) (second rest-numbers)))
+                                      (rest (rest rest-numbers))))
+           (evaluate-result-2 (concat [first-number]
+                                      (vector (merge-numbers (first rest-numbers) (second rest-numbers)))
+                                      (rest (rest rest-numbers))))))))
+
+ (defn solve2 [input]
+   (->> (parse-input input)
+        (filter evaluate-result-2)
+        (map first)
+        (reduce +)))
+
+ (solve2 sample-input)
+
+ (solve2 (slurp "resources/day07/input.txt"))
