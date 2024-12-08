a5c6d9cfcf3a6ab17f00548814e250530b1993c1
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-08 13:46:50 +0800

    add day08 part2

--- src/aoc2024/day08/solution.clj
+++ src/aoc2024/day08/solution.clj
@@ -58,6 +58,19 @@
   (and (< -1 x (count parsed-input))
        (< -1 y (count (first parsed-input)))))
 
+(defn get-all-antennas [parsed-input [[x1 y1] [x2 y2]]]
+  (loop [result []
+         multiplier 1]
+    (let [next-antenna [(+ x2 (* multiplier (- x2 x1)))
+                        (+ y2 (* multiplier (- y2 y1)))]]
+      (if (is-in-bounds? parsed-input next-antenna)
+        (recur (conj result next-antenna) (inc multiplier))
+        result))))
+
+(get-all-antennas (parse-input sample-input) [[1 8] [2 5]])
+(get-all-antennas (parse-input sample-input) [[1 1] [2 2]])
+(get-all-antennas (parse-input sample-input) [[2 2] [1 1]])
+
 (defn calculate-antennas [input]
   (let [parsed-input (parse-input input)
         frequencies (find-frequencies input)
@@ -71,7 +84,53 @@
          (filter (fn [antenna]
                    (is-in-bounds? parsed-input antenna)))
          count)))
-         
+
 (calculate-antennas sample-input)
 
 (calculate-antennas (slurp "resources/day08/input.txt"))
+
+
+;; part 2
+
+(defn find-antennas-for-positions [parsed-input positions]
+  (let [combinations (all-combinations positions)]
+    (->> combinations
+         (map (fn [combination]
+                (get-all-antennas parsed-input combination)))
+         (apply concat)
+         distinct)))
+
+(find-antennas-for-positions (parse-input sample-input) [[1 8] [2 5] [3 7] [4 4]])
+
+(defn calculate-antennas-2 [input]
+  (let [parsed-input (parse-input input)
+        frequencies (find-frequencies input)
+        grouped (group-positions-by-frequency frequencies)
+        frequency-positions (map :position frequencies)]
+    (->> grouped
+         vals
+         (map (fn [positions]
+                (find-antennas-for-positions parsed-input positions)))
+         (apply concat frequency-positions)
+         distinct
+         count)))
+
+
+(map :position (find-frequencies sample-input))
+
+(calculate-antennas-2 sample-input)
+
+(calculate-antennas-2 (slurp "resources/day08/input.txt"))
+
+(def test-input "T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+..........")
+
+(calculate-antennas-2 test-input)
