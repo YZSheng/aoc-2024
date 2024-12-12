2af23c7c4a488a242cea6404cc567a473302cac1
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-12 14:04:14 +0800

    add day12 part 2

--- src/aoc2024/day12/solution.clj
+++ src/aoc2024/day12/solution.clj
@@ -69,24 +69,24 @@ MMMISSJEEE")
            areas []]
       (cond
         (>= x height) areas
-        
+
         (>= y width) (recur (inc x) 0 visited areas)
-        
+
         (visited [x y]) (recur x (inc y) visited areas)
-        
+
         :else (let [area-coords (loop [to-visit #{[x y]}
-                                     area-visited #{}]
-                                (if (empty? to-visit)
-                                  area-visited
-                                  (let [current (first to-visit)
-                                        char (get-in parsed-input current)
-                                        neighbors (get-neighbors parsed-input (first current) (second current))
-                                        neighbors-with-char (filter #(= (:char %) char) neighbors)
-                                        neighbor-coords (map :coords neighbors-with-char)
-                                        unvisited-neighbors (remove area-visited neighbor-coords)]
-                                    (recur (into (disj to-visit current) unvisited-neighbors)
-                                           (conj area-visited current)))))]
-                (recur x 
+                                       area-visited #{}]
+                                  (if (empty? to-visit)
+                                    area-visited
+                                    (let [current (first to-visit)
+                                          char (get-in parsed-input current)
+                                          neighbors (get-neighbors parsed-input (first current) (second current))
+                                          neighbors-with-char (filter #(= (:char %) char) neighbors)
+                                          neighbor-coords (map :coords neighbors-with-char)
+                                          unvisited-neighbors (remove area-visited neighbor-coords)]
+                                      (recur (into (disj to-visit current) unvisited-neighbors)
+                                             (conj area-visited current)))))]
+                (recur x
                        (inc y)
                        (into visited area-coords)
                        (conj areas area-coords)))))))
@@ -119,3 +119,78 @@ MMMISSJEEE")
 (solve1 sample-input)
 (solve1 sample-input-large)
 (solve1 (slurp "resources/day12/input.txt"))
+
+;; part 2
+(defn find-edges [area-coords]
+  (let [coords (set area-coords)]
+    (->> (for [coord coords
+               edge-type [:top :bottom :left :right]
+               :let [[x y] coord
+                     neighbor (case edge-type
+                                :top    [(dec x) y]
+                                :bottom [(inc x) y]
+                                :left   [x (dec y)]
+                                :right  [x (inc y)])]
+               :when (not (coords neighbor))]
+           {:type edge-type :coord coord})
+         set)))
+
+(defn are-connected? [edge1 edge2]
+  (let [{type1 :type [x1 y1] :coord} edge1
+        {type2 :type [x2 y2] :coord} edge2]
+    (and
+     (= type1 type2)  ; must be the same type
+     (case type1
+       (:left :right)
+       (and (= y1 y2)                    ; same y coordinate
+            (= 1 (abs (- x1 x2))))       ; consecutive x coordinates
+       (:top :bottom)
+       (and (= x1 x2)                    ; same x coordinate
+            (= 1 (abs (- y1 y2))))))))   ; consecutive y coordinates
+
+(defn find-connected-group [edges edge]
+  (loop [to-check #{edge}
+         result #{}]
+    (if (empty? to-check)
+      result
+      (let [current (first to-check)
+            connected (set (filter #(and (not= % current)
+                                         (are-connected? current %))
+                                   edges))]
+        (recur (into (disj to-check current) (remove result connected))
+               (conj result current))))))
+
+(defn count-sides [edges]
+  (loop [remaining edges
+         groups []]
+    (if (empty? remaining)
+      (count groups)
+      (let [edge (first remaining)
+            group (find-connected-group remaining edge)]
+        (recur (apply disj remaining group)
+               (conj groups group))))))
+
+(find-edges #{[1 2] [2 2] [2 3] [3 3]})
+(count-sides (find-edges #{[1 2] [2 2] [2 3] [3 3]}))
+
+(are-connected? {:type :left :coord [1 2]} {:type :left :coord [1 3]})
+(are-connected? {:type :left :coord [1 2]} {:type :bottom :coord [1 3]})
+
+(def another-input "AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA")
+(find-connected-areas another-input)
+
+(defn solve2 [input]
+  (let [areas (find-connected-areas input)]
+    (->> areas
+         (map (fn [area]
+                (let [area-count (count area)]
+                  (* area-count (count-sides (find-edges area))))))
+         (reduce +))))
+
+(solve2 another-input)
+(solve2 (slurp "resources/day12/input.txt"))
