021bc1e228942cef382ba31d63e54f39103be971
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-15 16:34:19 +0800

    add day 15 part 2

--- src/aoc2024/day15/solution.clj
+++ src/aoc2024/day15/solution.clj
@@ -53,25 +53,27 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^")
                   [\# \. \. \. \. \O \. \. \. \#]
                   [\# \# \# \# \# \# \# \# \# \#]])
 
-sample-map
 (defn find-robot [m]
   (for [row (range (count m))
         cell (range (count (first m)))
         :when (= (get-in m [row cell]) \@)]
     [row cell]))
 
-(find-robot sample-map)
+(comment
 
-(find-robot [[\# \# \# \# \# \# \# \# \# \#]
-             [\# \. \. \O \. \. \O \. \O \#]
-             [\# \. \. \. \. \. \. \O \. \#]
-             [\# \. \O \O \. \. \O \. \O \#]
-             [\# \. \. \O \. \@ \. \O \. \#]
-             [\# \O \# \. \. \O \. \. \. \#]
-             [\# \O \. \. \O \. \. \O \. \#]
-             [\# \. \O \O \. \O \. \O \O \#]
-             [\# \. \. \. \. \O \. \. \. \#]
-             [\# \# \# \# \# \# \# \# \# \#]])
+  sample-map
+  (find-robot sample-map)
+
+  (find-robot [[\# \# \# \# \# \# \# \# \# \#]
+               [\# \. \. \O \. \. \O \. \O \#]
+               [\# \. \. \. \. \. \. \O \. \#]
+               [\# \. \O \O \. \. \O \. \O \#]
+               [\# \. \. \O \. \@ \. \O \. \#]
+               [\# \O \# \. \. \O \. \. \. \#]
+               [\# \O \. \. \O \. \. \O \. \#]
+               [\# \. \O \O \. \O \. \O \O \#]
+               [\# \. \. \. \. \O \. \. \. \#]
+               [\# \# \# \# \# \# \# \# \# \#]]))
 
 (defn get-connected-rocks [m pos direction]
   (loop [current-pos pos
@@ -97,23 +99,17 @@ sample-map
                    ">" (update robot-pos 1 inc))
         next-cell (get-in m next-pos)]
     (cond
-      ;; Hit a wall
       (= next-cell \#) m
 
-      ;; Empty space
       (= next-cell \.) (-> m
                            (assoc-in robot-pos \.)
                            (assoc-in next-pos \@))
 
-      ;; Hit a rock
       (= next-cell \O)
       (if-let [rocks (get-connected-rocks m next-pos direction)]
-        ;; Can move the chain of rocks
         (-> m
-            (assoc-in robot-pos \.)  ; clear robot's old position
-            ;; Clear all rock positions
+            (assoc-in robot-pos \.)
             (as-> m' (reduce #(assoc-in %1 %2 \.) m' rocks))
-            ;; Move all rocks one space in the direction
             (as-> m' (reduce #(assoc-in %1
                                         (case direction
                                           "v" (update %2 0 inc)
@@ -123,52 +119,36 @@ sample-map
                                         \O)
                              m'
                              rocks))
-            (assoc-in next-pos \@))  ; put robot in new position
-        ;; Can't move the chain
+            (assoc-in next-pos \@))
         m))))
 
-sample-map
+(comment
+  sample-map
 
-(move-robot sample-map [4 4] "v")
-(move-robot sample-map [4 4] "<")
+  (move-robot sample-map [4 4] "v")
+  (move-robot sample-map [4 4] "<")
 
-(find-robot [[\# \# \# \# \# \# \# \# \# \#]
-             [\# \. \. \O \. \. \O \. \O \#]
-             [\# \. \. \. \. \. \. \O \. \#]
-             [\# \. \O \O \. \. \O \. \O \#]
-             [\# \. \. \O \. \. \. \O \. \#]
-             [\# \O \# \@ \. \O \. \. \. \#]
-             [\# \O \. \. \O \. \. \O \. \#]
-             [\# \. \O \O \. \O \. \O \O \#]
-             [\# \. \. \. \. \O \. \. \. \#]
-             [\# \# \# \# \# \# \# \# \# \#]])
+  (find-robot [[\# \# \# \# \# \# \# \# \# \#]
+               [\# \. \. \O \. \. \O \. \O \#]
+               [\# \. \. \. \. \. \. \O \. \#]
+               [\# \. \O \O \. \. \O \. \O \#]
+               [\# \. \. \O \. \. \. \O \. \#]
+               [\# \O \# \@ \. \O \. \. \. \#]
+               [\# \O \. \. \O \. \. \O \. \#]
+               [\# \. \O \O \. \O \. \O \O \#]
+               [\# \. \. \. \. \O \. \. \. \#]
+               [\# \# \# \# \# \# \# \# \# \#]])
 
-(move-robot [[\# \# \# \# \# \# \# \# \# \#]
-             [\# \. \. \O \. \. \O \. \O \#]
-             [\# \. \. \. \. \. \. \O \. \#]
-             [\# \. \O \O \. \. \O \. \O \#]
-             [\# \. \. \O \. \. \. \O \. \#]
-             [\# \O \# \@ \. \O \. \. \. \#]
-             [\# \O \. \. \O \. \. \O \. \#]
-             [\# \. \O \O \. \O \. \O \O \#]
-             [\# \. \. \. \. \O \. \. \. \#]
-             [\# \# \# \# \# \# \# \# \# \#]] [5 3] "^")
-
-(str/split "<^^>>>vv<v>>v<<" #"")
-
-small-sample-input
-(parse-input small-sample-input)
-
-(let [parsed (parse-input small-sample-input)
-      parsed-map (:map parsed)
-      parsed-movement (:movement parsed)]
-  (reduce (fn [{:keys [m pos]} direction]
-            (let [new-m (move-robot m pos direction)]
-              {:m new-m
-               :pos (first (find-robot new-m))}))
-          {:m parsed-map
-           :pos (first (find-robot parsed-map))}
-          parsed-movement))
+  (move-robot [[\# \# \# \# \# \# \# \# \# \#]
+               [\# \. \. \O \. \. \O \. \O \#]
+               [\# \. \. \. \. \. \. \O \. \#]
+               [\# \. \O \O \. \. \O \. \O \#]
+               [\# \. \. \O \. \. \. \O \. \#]
+               [\# \O \# \@ \. \O \. \. \. \#]
+               [\# \O \. \. \O \. \. \O \. \#]
+               [\# \. \O \O \. \O \. \O \O \#]
+               [\# \. \. \. \. \O \. \. \. \#]
+               [\# \# \# \# \# \# \# \# \# \#]] [5 3] "^"))
 
 (defn find-boxes [m]
   (for [row (range (count m))
@@ -216,96 +196,204 @@ small-sample-input
                          [\# \. \. \. \. \. \#]
                          [\# \# \# \# \# \# \#]])
 
-part-2-sample-map
-
 (defn scale-map [m]
-  (mapv (fn [row] (vec (mapcat (fn [cell]
-                                 (case cell
-                                   \@ [\@ \.]
-                                   \. [\. \.]
-                                   \# [\# \#]
-                                   \O [\[ \]])) row))) m))
+  (if (string? m)
+    (scale-map (mapv vec (str/split-lines m)))
+    (mapv (fn [row]
+            (vec (mapcat (fn [cell]
+                           (case cell
+                             \@ [\@ \.]
+                             \. [\. \.]
+                             \# [\# \#]
+                             \O [\[ \]]))
+                         row)))
+          m)))
 
 (def scaled-part-2-sample-map (scale-map part-2-sample-map))
 
-scaled-part-2-sample-map
+(defn get-next-positions [pos direction]
+  (case direction
+    "v" (update pos 0 inc)
+    "^" (update pos 0 dec)
+    "<" (update pos 1 dec)
+    ">" (update pos 1 inc)))
+
+(defn is-block? [cell]
+  (contains? #{\[ \]} cell))
+
+(defn find-connected-blocks-move-vertically [next-pos m direction]
+  (loop [to-check #{next-pos}
+         blocks #{}]
+    (if (empty? to-check)
+      (vec blocks)
+      (let [check-pos (first to-check)
+            rest-to-check (disj to-check check-pos)
+            cell (get-in m check-pos)]
+        (if (is-block? cell)
+          (let [current-blocks (if (= cell \[)
+                                 [check-pos
+                                  [(first check-pos) (inc (second check-pos))]]
+                                 [check-pos
+                                  [(first check-pos) (dec (second check-pos))]])
+                next-row ((if (= direction "^") dec inc) (first check-pos))
+                next-positions [[next-row (second (first current-blocks))]
+                                [next-row (second (second current-blocks))]]]
+            (recur (into rest-to-check
+                         (filter #(is-block? (get-in m %)) next-positions))
+                   (into blocks current-blocks)))
+          (recur rest-to-check blocks))))))
+
+(defn find-connected-blocks-move-horizontally [next-pos m direction]
+  (loop [col (second next-pos)
+         blocks #{}]
+    (let [cell (get-in m [(first next-pos) col])]
+      (if (is-block? cell)
+        (let [pair-col (if (= cell \[) (inc col) (dec col))]
+          (recur ((if (= direction "<") dec inc) col)
+                 (conj blocks [(first next-pos) col]
+                       [(first next-pos) pair-col])))
+        (vec blocks)))))
 
 (defn get-connected-rocks-with-double-space-blocks [m pos direction]
-  (let [is-block? #(contains? #{\[ \]} %)
-        next-pos (case direction
-                  "v" (update pos 0 inc)
-                  "^" (update pos 0 dec)
-                  "<" (update pos 1 dec)
-                  ">" (update pos 1 inc))
+  (let [next-pos (get-next-positions pos direction)
         current-cell (get-in m next-pos)]
     (when (is-block? current-cell)
-      (let [start-row (first next-pos)
-            start-col (second next-pos)
-            ;; Find all blocks in a given row that are horizontally connected
-            find-row-blocks (fn [row col]
-                            (loop [current-col col
-                                  blocks #{[row col]}
-                                  visited #{col}]
-                              (let [left-col (dec current-col)
-                                    right-col (inc current-col)
-                                    left-cell (get-in m [row left-col])
-                                    right-cell (get-in m [row right-col])]
-                                (cond
-                                  (and (not (visited left-col))
-                                       (is-block? left-cell)) (recur left-col 
-                                                                    (conj blocks [row left-col])
-                                                                    (conj visited left-col))
-                                  (and (not (visited right-col))
-                                       (is-block? right-cell)) (recur right-col 
-                                                                     (conj blocks [row right-col])
-                                                                     (conj visited right-col))
-                                  :else blocks))))
-            ;; First find vertically connected blocks
-            initial-blocks (case direction
-                           "^" (let [above-row (dec start-row)]
-                                (if (is-block? (get-in m [above-row start-col]))
-                                  #{[start-row start-col] [above-row start-col]}
-                                  #{[start-row start-col]}))
-                           "v" (let [below-row (inc start-row)]
-                                (if (is-block? (get-in m [below-row start-col]))
-                                  #{[start-row start-col] [below-row start-col]}
-                                  #{[start-row start-col]}))
-                           (">" "<") #{[start-row start-col]})
-            ;; Then find all horizontally connected blocks for each row involved
-            connected-blocks (reduce (fn [acc [row col]]
-                                     (into acc (find-row-blocks row col)))
-                                   #{}
-                                   initial-blocks)]
-        (vec connected-blocks)))))
+      (if (#{"^" "v"} direction)
+        (find-connected-blocks-move-vertically next-pos m direction)
+        (find-connected-blocks-move-horizontally next-pos m direction)))))
 
-(find-robot scaled-part-2-sample-map)
+(comment
+  part-2-sample-map
+  scaled-part-2-sample-map
+  (find-robot scaled-part-2-sample-map)
 
+  scaled-part-2-sample-map
+
+  (find-robot scaled-part-2-sample-map)
+
+  (get-connected-rocks-with-double-space-blocks scaled-part-2-sample-map (first (find-robot scaled-part-2-sample-map)) "<")
+
+  (def part-2-connected-blocks-map [[\# \# \# \# \# \# \# \# \# \# \# \# \# \#]
+                                    [\# \# \. \. \. \. \. \. \# \# \. \. \# \#]
+                                    [\# \# \. \. \. \. \. \. \. \. \. \. \# \#]
+                                    [\# \# \. \. \. \. \[ \] \[ \] \. \. \# \#]
+                                    [\# \# \. \. \. \. \[ \] \. \. \. \. \# \#]
+                                    [\# \# \. \. \. \. \. \@ \. \. \. \. \# \#]
+                                    [\# \# \# \# \# \# \# \# \# \# \# \# \# \#]])
+
+  part-2-connected-blocks-map
+  (find-robot part-2-connected-blocks-map)
+  (get-connected-rocks-with-double-space-blocks part-2-connected-blocks-map [5 7] "^")
+
+  (def part-2-connected-complex-blocks-map [[\# \# \# \# \# \# \# \# \# \# \# \# \# \#]
+                                            [\# \# \. \. \. \. \. \. \# \# \. \. \# \#]
+                                            [\# \# \. \. \. \. \. \. \. \. \. \. \# \#]
+                                            [\# \# \. \. \. \[ \] \[ \] \. \. \. \# \#]
+                                            [\# \# \. \. \. \. \[ \] \. \. \. \. \# \#]
+                                            [\# \# \. \. \. \. \. \@ \. \. \. \. \# \#]
+                                            [\# \# \# \# \# \# \# \# \# \# \# \# \# \#]])
+
+  part-2-connected-complex-blocks-map
+  (find-robot part-2-connected-complex-blocks-map)
+  (get-connected-rocks-with-double-space-blocks part-2-connected-complex-blocks-map [5 7] "^")
+
+  (get-connected-rocks-with-double-space-blocks [[\# \# \# \# \# \# \# \# \# \# \# \# \# \#]
+                                                 [\# \# \. \. \. \. \. \. \# \# \. \. \# \#]
+                                                 [\# \# \. \. \. \[ \] \[ \] \. \. \. \# \#]
+                                                 [\# \# \. \. \. \@ \[ \] \. \. \. \. \# \#]
+                                                 [\# \# \. \. \. \. \. \. \. \. \. \. \# \#]
+                                                 [\# \# \. \. \. \. \. \. \. \. \. \. \# \#]
+                                                 [\# \# \# \# \# \# \# \# \# \# \# \# \# \#]] [3 5] "^"))
+
+(defn valid-position? [m pos]
+  (and pos
+       (>= (first pos) 0)
+       (>= (second pos) 0)
+       (< (first pos) (count m))
+       (< (second pos) (count (first m)))))
+
+(defn get-new-rock-positions [m rocks direction]
+  (map (fn [pos]
+         (let [new-pos (case direction
+                         "v" (update pos 0 inc)
+                         "^" (update pos 0 dec)
+                         "<" (update pos 1 dec)
+                         ">" (update pos 1 inc))
+               target-cell (get-in m new-pos)]
+           (when (and (valid-position? m new-pos)
+                      (not= target-cell \#))
+             [pos new-pos])))
+       rocks))
+
+(defn move-all-connected-blocks [m robot-pos next-pos direction]
+  (if-let [rocks (seq (get-connected-rocks-with-double-space-blocks m robot-pos direction))]
+    (let [new-rock-positions (get-new-rock-positions m rocks direction)
+          all-valid? (every? some? new-rock-positions)]
+      (if all-valid?
+        (-> m
+            (assoc-in robot-pos \.)
+            (as-> m' (reduce #(assoc-in %1 (first %2) \.) m' new-rock-positions))
+            (as-> m' (reduce #(assoc-in %1 (second %2) (get-in m (first %2)))
+                             m'
+                             new-rock-positions))
+            (assoc-in next-pos \@))
+        m))
+    m))
+
+(defn move-robot-connected-blocks [m robot-pos direction]
+  (let [next-pos (case direction
+                   "v" (update robot-pos 0 inc)
+                   "^" (update robot-pos 0 dec)
+                   "<" (update robot-pos 1 dec)
+                   ">" (update robot-pos 1 inc))
+        next-cell (get-in m next-pos)]
+    (cond
+      (= next-cell \.)
+      (-> m
+          (assoc-in robot-pos \.)
+          (assoc-in next-pos \@))
+
+      (contains? #{\[ \]} next-cell)
+      (move-all-connected-blocks m robot-pos next-pos direction)
+
+      (= next-cell \#) m
+
+      :else m)))
+
+(move-robot-connected-blocks part-2-connected-complex-blocks-map [5 7] "^")
 scaled-part-2-sample-map
-
 (find-robot scaled-part-2-sample-map)
+(move-robot-connected-blocks scaled-part-2-sample-map [3 10] "<")
 
-(get-connected-rocks-with-double-space-blocks scaled-part-2-sample-map (first (find-robot scaled-part-2-sample-map)) "<")
+(defn find-boxes-left-bracket [m]
+  (for [row (range (count m))
+        cell (range (count (first m)))
+        :when (= (get-in m [row cell]) \[)]
+    [row cell]))
 
-(def part-2-connected-blocks-map [[\# \# \# \# \# \# \# \# \# \# \# \# \# \#]
-                                  [\# \# \. \. \. \. \. \. \# \# \. \. \# \#]
-                                  [\# \# \. \. \. \. \. \. \. \. \. \. \# \#]
-                                  [\# \# \. \. \. \. \[ \] \[ \] \. \. \# \#]
-                                  [\# \# \. \. \. \. \[ \] \. \. \. \. \# \#]
-                                  [\# \# \. \. \. \. \. \@ \. \. \. \. \# \#]
-                                  [\# \# \# \# \# \# \# \# \# \# \# \# \# \#]])
+(find-boxes-left-bracket part-2-connected-complex-blocks-map)
 
-part-2-connected-blocks-map
-(find-robot part-2-connected-blocks-map)
-(get-connected-rocks-with-double-space-blocks part-2-connected-blocks-map [5 7] "^")
+(defn solve2 [input]
+  (let [parsed (parse-input input)
+        parsed-map (scale-map (:map parsed))
+        parsed-movement (:movement parsed)
+        initial-pos (first (find-robot parsed-map))
+        result (reduce (fn [{:keys [m pos]} direction]
+                         (if (nil? pos)
+                           {:m m :pos (first (find-robot m))}
+                           (let [new-m (move-robot-connected-blocks m pos direction)
+                                 new-pos (first (find-robot new-m))]
+                             {:m new-m
+                              :pos new-pos})))
+                       {:m parsed-map
+                        :pos initial-pos}
+                       parsed-movement)
 
-(def part-2-connected-complex-blocks-map [[\# \# \# \# \# \# \# \# \# \# \# \# \# \#]
-                                          [\# \# \. \. \. \. \. \. \# \# \. \. \# \#]
-                                          [\# \# \. \. \. \. \. \. \. \. \. \. \# \#]
-                                          [\# \# \. \. \. \[ \] \[ \] \. \. \. \# \#]
-                                          [\# \# \. \. \. \. \[ \] \. \. \. \. \# \#]
-                                          [\# \# \. \. \. \. \. \@ \. \. \. \. \# \#]
-                                          [\# \# \# \# \# \# \# \# \# \# \# \# \# \#]])
+        boxes (find-boxes-left-bracket (:m result))]
+    (->> boxes
+         (map (fn [[x y]] (+ (* 100 x) y)))
+         (reduce +))))
 
-part-2-connected-complex-blocks-map
-(find-robot part-2-connected-complex-blocks-map)
-(get-connected-rocks-with-double-space-blocks part-2-connected-complex-blocks-map [5 7] "^")
+(solve2 small-sample-input)
+(solve2 sample-input)
+(solve2 (slurp "resources/day15/input.txt"))
