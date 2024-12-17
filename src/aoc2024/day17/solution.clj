9ac59713244f8a0e450bce52de9ac8e81a8a498a
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-17 20:32:32 +0800

    add day 17 part 2

--- src/aoc2024/day17/solution.clj
+++ src/aoc2024/day17/solution.clj
@@ -1,17 +1,10 @@
-(ns aoc2024.day17.solution
-  (:require [clojure.string :as str]))
+(ns aoc2024.day17.solution)
 
 ;; Combo operands 0 through 3 represent literal values 0 through 3.
 ;; Combo operand 4 represents the value of register A.
 ;; Combo operand 5 represents the value of register B.
 ;; Combo operand 6 represents the value of register C.
 ;; Combo operand 7 is reserved and will not appear in valid programs.
-
-(def sample-input "Register A: 729
-Register B: 0
-Register C: 0
-
-Program: 0,1,5,4,3,0")
 
 (def op-map {0 0
              1 1
@@ -22,7 +15,6 @@ Program: 0,1,5,4,3,0")
              6 :C
              7 nil})
 
-(op-map 6)
 (defn get-combo-value [m value]
   (let [op-map-value (op-map value)]
     (case op-map-value
@@ -33,15 +25,12 @@ Program: 0,1,5,4,3,0")
       3 3
       (get m op-map-value))))
 
-
 (defn adv [m arg]
   (let [combo-value (get-combo-value m arg)
         numerator (get m :A)
         denominator (Math/pow 2 combo-value)
         result (long (quot numerator denominator))]
     (assoc m :A result)))
-
-(adv {:A 2024} 1)
 
 (defn bxl [m arg]
   (let [register-b (get m :B)
@@ -53,19 +42,14 @@ Program: 0,1,5,4,3,0")
         module-8 (mod combo-value 8)]
     (assoc m :B module-8)))
 
-; {:C 9, :B 1}
-(bst {:C 9} 6)
-
 (defn update-instructions [instructions value]
   (subvec instructions value))
 
-(update-instructions [1 2 3 4 5 6 7 8 9 10] 2)
-
-(defn jnz [m value latest-instructions original-instructions]
+(defn jnz [m value latest-instructions original-instructions result]
   (let [a-register (get m :A)]
     (if (zero? a-register)
-      [m (drop 2 latest-instructions)]
-      [m (update-instructions original-instructions value)])))
+      [m (drop 2 latest-instructions) result]
+      [m (update-instructions original-instructions value) result])))
 
 (defn bxc [m value]
   (let [b-register (get m :B)
@@ -73,14 +57,10 @@ Program: 0,1,5,4,3,0")
         bitwise-xor (bit-xor b-register c-register)]
     (assoc m :B bitwise-xor)))
 
-(defn out [m value]
+(defn out [m value result]
   (let [combo-value (get-combo-value m value)
         module-8 (mod combo-value 8)]
-    (do
-      (println "Output::" module-8)
-      m)))
-
-(out {:A 10} 0)
+    [m (conj result module-8)]))
 
 
 (defn bdv [m arg]
@@ -97,37 +77,63 @@ Program: 0,1,5,4,3,0")
         result (long (quot numerator denominator))]
     (assoc m :C result)))
 
-(defn process-single-instruction [m [op-code arg] latest-instructions original-instructions]
+(defn process-single-instruction [m [op-code arg] latest-instructions original-instructions result]
   (case op-code
-    0 [(adv m arg) (drop 2 latest-instructions)]
-    1 [(bxl m arg) (drop 2 latest-instructions)]
-    2 [(bst m arg) (drop 2 latest-instructions)]
-    3 (jnz m arg latest-instructions original-instructions)
-    4 [(bxc m arg) (drop 2 latest-instructions)]
-    5 [(out m arg) (drop 2 latest-instructions)]
-    6 [(bdv m arg) (drop 2 latest-instructions)]
-    7 [(cdv m arg) (drop 2 latest-instructions)]))
+    0 [(adv m arg) (drop 2 latest-instructions) result]
+    1 [(bxl m arg) (drop 2 latest-instructions) result]
+    2 [(bst m arg) (drop 2 latest-instructions) result]
+    3 (jnz m arg latest-instructions original-instructions result)
+    4 [(bxc m arg) (drop 2 latest-instructions) result]
+    5 (let [[updated-m updated-result] (out m arg result)]
+        [updated-m (drop 2 latest-instructions) updated-result])
+    6 [(bdv m arg) (drop 2 latest-instructions) result]
+    7 [(cdv m arg) (drop 2 latest-instructions) result]))
 
 (defn process-instructions [m original-instructions]
   (loop [m m
-         instructions original-instructions]
+         instructions original-instructions
+         result []]
     (if (empty? instructions)
-      m
-      (let [[new-m new-instructions]
-            (process-single-instruction m [(first instructions) (second instructions)] instructions original-instructions)]
-        (recur new-m new-instructions)))))
+      [m result]
+      (let [[new-m new-instructions new-result]
+            (process-single-instruction m [(first instructions) (second instructions)] instructions original-instructions result)]
+        (recur new-m new-instructions new-result)))))
 
-(process-instructions {:C 9} [2 6])
-(process-instructions {:A 10} [5 0])
-(process-instructions {:A 10} [5 1])
-(process-instructions {:A 10} [5 2])
-(process-instructions {:A 10} [5 0 5 1 5 2])
+(comment
+  (process-instructions {:C 9} [2 6])
+  (process-instructions {:A 10} [5 0])
+  (process-instructions {:A 10} [5 1])
+  (process-instructions {:A 10} [5 2])
+  (process-instructions {:A 10} [5 0 5 1 5 2])
 
-(process-instructions {:A 2024} [0 1 5 4 3 0])
+  (process-instructions {:A 2024} [0 1 5 4 3 0])
 
-(process-instructions {:B 29} [1 7])
+  (process-instructions {:B 29} [1 7])
 
-(process-instructions {:B 2024 :C 43690} [4 0])
+  (process-instructions {:B 2024 :C 43690} [4 0])
 
-(process-instructions {:A 729 :B 0 :C 0} [0 1 5 4 3 0])
-(process-instructions {:A 64854237 :B 0 :C 0} [2 4 1 1 7 5 1 5 4 0 5 5 0 3 3 0])
+  (process-instructions {:A 729 :B 0 :C 0} [0 1 5 4 3 0])
+  (process-instructions {:A 64854237 :B 0 :C 0} [2 4 1 1 7 5 1 5 4 0 5 5 0 3 3 0]))
+
+;; part 2
+(process-instructions {:A 117440 :B 0 :C 0} [0 3 5 4 3 0])
+
+(defn solve2 [result]
+  (let [rprogram (reverse result)]
+    (loop [power (- (count result) 1)
+           open  #{(- (bit-shift-left 1 (* 3 (count result))) 1)}]
+      (if (< power 0)
+        (apply min open)
+        (recur (dec power)
+               (mapcat (fn [o]
+                         (for [i (take 8 (iterate #(- % (bit-shift-left 1 (* 3 power))) o))
+                               :let  [output (->> result
+                                                  (process-instructions {:A i :B 0 :C 0})
+                                                  last
+                                                  reverse)]
+                               :when (= (take (- (count result) power) output)
+                                        (take (- (count result) power) rprogram))]
+                           i)) open))))))
+
+(solve2 [0 3 5 4 3 0])
+(solve2 [2 4 1 1 7 5 1 5 4 0 5 5 0 3 3 0])
