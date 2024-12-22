b503b221a3fcfed30498ced507dfe58a5782c987
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-12-22 14:27:13 +0800

    Add day 22 part 2: Implement functions for calculating one-digit prices, generating price change maps, and solving the price change problem. Enhance input parsing and result aggregation for improved functionality.

--- src/aoc2024/day22/solution.clj
+++ src/aoc2024/day22/solution.clj
@@ -87,3 +87,106 @@
 
 (solve1 sample-input)
 (solve1 (slurp "resources/day22/input.txt"))
+
+;; part 2
+
+(defn to-one-digit [num]
+  (Character/digit (last (str num)) 10))
+
+(to-one-digit 123)
+(to-one-digit 100010)
+
+(defn calculate-price [times initial-price]
+  (->> initial-price
+       (calculate-secret-number times)
+       (to-one-digit)))
+
+(calculate-price 2000 123)
+
+(loop [n 0
+       secret-number 123
+       result [(to-one-digit 123)]]
+  (if (< n (dec 10))
+    (let [updated-secret-number (ops secret-number)
+          updated-result (conj result (to-one-digit updated-secret-number))]
+      (recur (inc n) updated-secret-number updated-result))
+    result))
+
+(defn generate-one-digit-prices [times initial-price]
+  (loop [n 0
+         secret-number initial-price
+         result [(to-one-digit initial-price)]]
+    (if (< n (dec times))
+      (recur (inc n) (ops secret-number) (conj result (to-one-digit (ops secret-number))))
+      result)))
+
+(defn into-price-change-map [xs]
+  (reduce (fn [acc [k v]]
+            (if (contains? acc k)
+              acc
+              (assoc acc k v)))
+          {}
+          xs))
+
+(defn group-sell-price-by-price-change [secret-number-times original-secret-number]
+  (->> (generate-one-digit-prices secret-number-times original-secret-number)
+       (partition 2 1)
+       (map (fn [[from to]] [from to (- to from)]))
+       (partition 4 1)
+       (group-by (fn [xs] (mapv last xs)))
+       (map (fn [[k v]] [k (->> v first last second)]))
+       (into-price-change-map)))
+
+(defn get-all-price-changes [input n]
+  (->> input
+       (parse-input)
+       (map #(group-sell-price-by-price-change n %))
+       (mapcat (fn [m] (keys m)))
+       distinct))
+
+(defn solve2 [input n]
+  (let [sell-price-maps (->> input
+                            (parse-input)
+                            (map #(group-sell-price-by-price-change n %)))
+        price-sums (->> sell-price-maps
+                       (apply merge-with +))]
+    (->> price-sums
+         (vals)
+         (apply max))))
+
+(def part-2-sample-input "1
+2
+3
+2024")
+
+(solve2 part-2-sample-input 2000)
+(solve2 (slurp "resources/day22/input.txt") 2000)
+
+(comment
+
+  (generate-one-digit-prices 10 123)
+  (get-all-price-changes sample-input 10)
+
+  (->> sample-input
+       (parse-input)
+       (map #(group-sell-price-by-price-change 10 %))
+       (mapcat (fn [m] (keys m)))
+       distinct)
+
+  (->> (generate-one-digit-prices 10 123)
+       (partition 2 1)
+       (map (fn [[from to]] [from to (- to from)]))
+       (partition 4 1)
+       (group-by (fn [xs] (mapv last xs)))
+       (map (fn [[k v]] [k (->> v first last second)])))
+
+;; conver this into a map without overriding [1 2] key if it's set
+;; '([[1 2] 3] [[5 6] 7] [[1 2] 4])
+;; {[1 2] 3, [5 6] 7}
+  (reduce (fn [acc [k v]]
+            (if (contains? acc k)
+              acc
+              (assoc acc k v)))
+          {}
+          '([[1 2] 3] [[5 6] 7] [[1 2] 4]))
+  (into-price-change-map '([[-3 6 -1 -1] 4] [[6 -1 -1 0] 4] [[-1 -1 0 2] 6] [[-1 0 2 -2] 4] [[0 2 -2 0] 4] [[2 -2 0 -2] 2])))
